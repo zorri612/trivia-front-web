2ad46794968ea0d7336ff947220baf2c1777542c
MozartCuidadorDigital <zorrillanico200312@gmail.com>
2025-09-20 00:48:16 -0500

    postcc

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 👈 MUY importante para que Tailwind lea tus componentes
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
